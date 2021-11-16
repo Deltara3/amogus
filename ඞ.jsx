a0995feb3cbe62a059efb842b4325c74b42a2150
unkn0wncvm1 <87480097+unkn0wncvm1@users.noreply.github.com>
2021-11-16 22:59:47 +0300

    fix typo

--- ඞ.jsx
+++ ඞ.jsx
@@ -10,5 +10,5 @@ class HelloMessage extends React.Component {
 
 ReactDOM.render(
   <HelloMessage name="ඞ amogus" />,
-  document.getElementById('hello-example')
+  document.getElementById('root')
 );
